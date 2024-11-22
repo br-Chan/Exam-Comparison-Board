cdd3073fdc21d37525f3ea9f3bba04fb874503b5
Michael Boyer <mboy221@aucklanduni.ac.nz>
2024-11-22 14:44:54 +1300

    fixed rounding and overall colours

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <div className=" rounded flex bg-slate-700 w-1/2 h-screen m-3">
+      <div className=" flex w-1/2 h-screen m-3" style={{ borderRadius: '30px', backgroundColor: '#2a2a2a' }}>
         <Link href="/login">login </Link>
       </div>
     </div>
