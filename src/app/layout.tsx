4b74e55dc2e2f96f1ea3f35e4846e71d68db49a1
harbassan <harbassan@hotmail.com>
2024-12-02 13:30:08 +1300

    feat: add main landing content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,16 +3,16 @@ import localFont from "next/font/local";
 import "./globals.css";
 import Header from "./components/header";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
 
 const lexend = localFont({
   src: "./fonts/Lexend-VariableFont_wght.ttf",
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,24 @@
 "use client";
 
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <div>
-        <Link href="/login">login </Link>
+      <div className="fixed -z-50 right-0 bottom-0 h-full w-1/2">
+        <Image src="/background.png" fill={true} alt="" />
+      </div>
+      <div className="flex px-20 font-[family-name:var(--font-lexend)] flex-wrap">
+        <div className="flex flex-col flex-grow gap-10">
+          <div className="text-8xl">
+            <h1>Welcome.</h1>
+          </div>
+          <Link href="/login" className="bg-blue-600 w-fit text-xl px-8 py-3 rounded-full">LOGIN </Link>
+        </div>
+        <div className="w-80 backdrop-blur-sm p-6 rounded-xl">
+          <p>Our goal is to help students around the world study for their exams, by providing a platform that encourages collaboration to solve previous exams. </p>
+        </div>
       </div>
     </div>
   );
