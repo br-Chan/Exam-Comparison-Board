36c87f8bfe1afab45a08332d338bd5b4c744fb19
harbassan <harbassan@hotmail.com>
2024-12-17 17:12:11 +1300

    feat: make home page mobile functional

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <nav className="flex pt-10 px-12 lg:px-20 xl:px-28 justify-between w-full font-[family-name:var(--font-lexend)] h-25">
+    <nav className="flex pt-10 px-12 justify-between w-full font-[family-name:var(--font-lexend)] h-25">
       <div className="flex items-end gap-2">
         <div className="relative w-36 h-32">
           <Link href="/">
@@ -11,23 +11,23 @@ export default function Header() {
           </Link>
         </div>
       </div>
-      <ul className="flex gap-20">
-        <li>
-          <Link href="/search">
-            <h1 className="">SEARCH</h1>
-          </Link>
-        </li>
-        <li>
-          <Link href="/homepage">
-            <h1 className="">HOMEPAGE</h1>
-          </Link>
-        </li>
-        <li>
-          <Link href="/enquiries">
-            <h1 className="">ENQUIRIES</h1>
-          </Link>
-        </li>
-      </ul>
+      {/* <ul className="flex gap-20"> */}
+      {/*   <li> */}
+      {/*     <Link href="/search"> */}
+      {/*       <h1 className="">SEARCH</h1> */}
+      {/*     </Link> */}
+      {/*   </li> */}
+      {/*   <li> */}
+      {/*     <Link href="/homepage"> */}
+      {/*       <h1 className="">HOMEPAGE</h1> */}
+      {/*     </Link> */}
+      {/*   </li> */}
+      {/*   <li> */}
+      {/*     <Link href="/enquiries"> */}
+      {/*       <h1 className="">ENQUIRIES</h1> */}
+      {/*     </Link> */}
+      {/*   </li> */}
+      {/* </ul> */}
     </nav>
   );
 }
