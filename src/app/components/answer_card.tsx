b14ee3f43750686ac2444e924165d1b6d2d457bc
harbassan <harbassan@hotmail.com>
2024-12-21 18:51:36 +1300

    feat: add answer card component

---
+++ src/app/components/answer_card.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image"
+
+import tick from "../../../public/tick.svg"
+import cross from "../../../public/cross.svg"
+
+export default function AnswerCard({ author, content, votes }: { author: string, content: string, votes: { up: number, down: number } }) {
+  return (
+    <div className=" bg-neutral-900 p-5 rounded-lg w-72 flex flex-col gap-2">
+      <h2 className=" text-lg">{author}</h2>
+      <p className=" text-sm">{content}</p>
+      <div className=" text-md flex gap-5">
+        <button className=" flex items-center gap-1">
+          <Image src={tick} width={20} height={20} alt="tick" className=" invert" />
+          <span>{votes.up}</span>
+        </button>
+        <button className=" flex items-center gap-1">
+          <Image src={cross} width={20} height={20} alt="cross" className=" invert" />
+          <span>{votes.down}</span>
+        </button>
+      </div>
+    </div>
+  )
+}
